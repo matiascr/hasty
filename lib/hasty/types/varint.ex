d1183b981ef79146f2a3199d347d5191f08c65d6
matiascr <carlander.matias@gmail.com>
2025-06-30 15:48:37 +0200

    Implemented variable length integer encode, decode and length

---
+++ lib/hasty/types/varint.ex
@@ -0,0 +1,63 @@
+defmodule Hasty.Types.VarInt do
+  @moduledoc false
+
+  @type varint() :: binary()
+
+  @size_00 2 ** 6
+  @size_01 2 ** 14
+  @size_10 2 ** 30
+  @size_11 2 ** 62
+
+  @doc """
+  Returns the length of the given varint (in number of bytes).
+
+  # Examples
+
+        iex> Hasty.Types.VarInt.length(<<0x12>>)
+        1
+
+        iex> Hasty.Types.VarInt.length(<<0x81, 0x0C, 0xA0, 0x00>>)
+        4
+  """
+  @spec length(varint()) :: pos_integer()
+  def length(<<0b00::2, _::bitstring>>), do: 1
+  def length(<<0b01::2, _::bitstring>>), do: 2
+  def length(<<0b10::2, _::bitstring>>), do: 4
+  def length(<<0b11::2, _::bitstring>>), do: 8
+  def length(_), do: raise(ArgumentError)
+
+  @doc """
+  Encodes a given integer into the QUIC variable length integer format.
+
+  # Examples
+
+        iex> encode(12)
+        <<0b00::2, 12::6>>
+
+        iex> encode(64)
+        <<0b01::2, 64::14>>
+  """
+  @spec encode(non_neg_integer()) :: binary()
+  def encode(n) when is_integer(n) and n > 0 and n < @size_00, do: <<0b00::2, n::6>>
+  def encode(n) when is_integer(n) and n > 0 and n < @size_01, do: <<0b01::2, n::14>>
+  def encode(n) when is_integer(n) and n > 0 and n < @size_10, do: <<0b10::2, n::30>>
+  def encode(n) when is_integer(n) and n > 0 and n < @size_11, do: <<0b11::2, n::62>>
+
+  @doc """
+  Decodes a given variable length integer into the value contained and the rest of the binary given.
+
+  # Examples
+
+        iex> decode(<<0b00::2, 12::6>>)
+        {12, <<>>}
+
+        iex> decode(<<0b01::2, 64::14, 0x123::8>>)
+        {64, <<0x123::8>>}
+  """
+  @spec decode(varint()) :: {pos_integer(), binary()}
+  def decode(<<0b00::2, n::6, rest::binary>>), do: {n, rest}
+  def decode(<<0b01::2, n::14, rest::binary>>), do: {n, rest}
+  def decode(<<0b10::2, n::30, rest::binary>>), do: {n, rest}
+  def decode(<<0b11::2, n::62, rest::binary>>), do: {n, rest}
+  def decode(_), do: raise(ArgumentError)
+end
--- test/hasty_test.exs
+++ test/hasty_test.exs
@@ -2,4 +2,5 @@ defmodule HastyTest do
   use ExUnit.Case
 
   doctest Hasty
+  doctest Hasty.Types.VarInt, import: true
 end
